97d1bcc5298b5b618c5113b6e9a959bd992905e9
Hajime Ishikawa <ishikawa@nttpc.co.jp>
2017-11-09 10:04:01 +0000

    コメント機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,15 +7,49 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to tweet_path(@tweet), notice: 'コメントを投稿しました。' }
+        # JS形式でレスポンスを返します。
+        format.js { render :index }
       else
         format.html { render :new }
       end
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @tweet = @comment.tweet
+    # クライアント要求に応じてフォーマットを変更
+    respond_to do |format|
+      if @comment.destroy
+        format.html { redirect_to tweet_path(@blog), notice: 'コメントを削除しました。' }
+        # JS形式でレスポンスを返します。
+        format.js { render :index }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+    #binding.pry
+    @comment = Comment.find(params[:id])
+    @tweet = @comment.tweet
+  end
+  
+  def update
+    #binding.pry
+    set_comment
+    @comment.update(comment_params)
+    redirect_to tweet_path(@comment.tweet), notice: "コメントを更新しました"
+  end
+
   private
     # ストロングパラメーター
     def comment_params
       params.require(:comment).permit(:tweet_id, :content)
+    end
+    
+    def set_comment
+      @comment=Comment.find(params[:id])
     end
 end
