bf44ab65d89b347e9c56863143b17332e97bbbcc
Hajime Ishikawa <ishikawa@nttpc.co.jp>
2017-11-12 04:12:43 +0000

    models/tweet.rb 修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 class Tweet < ActiveRecord::Base
-    validates :content, length: { in: 0..140 }
+    # validates :content, length: { in: 0..140 }
+    validates :content, presence: true
     
     belongs_to :user
     has_many :comments, dependent: :destroy
