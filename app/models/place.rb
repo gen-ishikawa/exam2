e6debbc6a908f05779bed4c112a7f405f9fdd68a
Hajime Ishikawa <ishikawa@nttpc.co.jp>
2017-11-16 11:16:53 +0000

    詳細画面

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
+    validates :name, :description, :latitude, :longitude, presence: true
+    
     belongs_to :user
     has_many :placecomments, dependent: :destroy
 end
