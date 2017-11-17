c7f3f056dfb9f4a874f46c400e7878523c835890
Hajime Ishikawa <ishikawa@nttpc.co.jp>
2017-11-17 07:48:10 +0000

    validates models/place.rb

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ActiveRecord::Base
-    validates :name, :description, :latitude, :longitude, presence: true
+    validates :name, :description, presence: true
+    validates :latitude, format: { with: /\A35\.?\d{0,6}\z/ }
+    validates :longitude, format: { with: /\A139|140\.?\d{0,6}\z/ }
     
     belongs_to :user
     has_many :placecomments, dependent: :destroy
