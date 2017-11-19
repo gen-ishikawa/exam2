c21627d7eadf2073bf4609fabacfbc2a074c3b9e
Hajime Ishikawa <ishikawa@nttpc.co.jp>
2017-11-19 09:57:26 +0000

    app/models/place.rb

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,8 @@
 class Place < ActiveRecord::Base
-    validates :name, :description, presence: true
-    validates :latitude, format: { with: /\A35\.?\d{0,6}\z/ }
-    validates :longitude, format: { with: /\A139|140\.?\d{0,6}\z/ }
+    #validates :name, :description, presence: true
+    validates :name, :description, :latitude, :longitude, presence: true
+    validates :latitude, format: { with: /\A35\.?\d{0,6}\z/ }, allow_blank: true
+    validates :longitude, format: { with: /\A139|140\.?\d{0,6}\z/ }, allow_blank: true
     
     belongs_to :user
     has_many :placecomments, dependent: :destroy
