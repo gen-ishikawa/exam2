79f70a10b5a5bf14726c46c2a4fc4a7c1f3ab5ae
Hajime Ishikawa <ishikawa@nttpc.co.jp>
2017-11-19 12:05:14 +0000

    余計なディレクトリ削除

--- app/mailers/models/placecomment.rb
+++
@@ -1,6 +0,0 @@
-class Placecomment < ActiveRecord::Base
-  #validates :content, presence: true
-
-  belongs_to :user
-  belongs_to :place
-end
--- app/mailers/models/tweet.rb
+++
@@ -1,9 +0,0 @@
-class Tweet < ActiveRecord::Base
-    # validates :content, length: { in: 0..140 }
-    validates :content, presence: true
-    
-    belongs_to :user
-    has_many :comments, dependent: :destroy
-    
-    mount_uploader :image, AvatarUploader
-end
